991b536a93b42baef001071410976ff1fcecc4e8
Riptide <mribr9@gmail.com>
2017-07-01 14:44:56 -0700

    Added package.json

--- Desktop/baconbot2/index.js
+++ Desktop/baconbot2/index.js
@@ -655,7 +655,7 @@ const app = express();
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
-const port = process.env.PORT || 3232;
+const port = process.env.PORT || 8080;
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
@@ -666,14 +666,13 @@ app.use(express.static(__dirname + '/public'));
 // set the home page route
 app.get('/', (request, response) => {
     // ejs render automatically looks in the views folder
-    response.render('index.js');
+    response.render('index');
+});
 
      // pings server every 15 minutes to prevent dynos from sleeping
 setInterval(() => {
   http.get('http://baconbot2.herokuapp.com');
 }, 900000);
-
-});
 
 app.listen(port, () => {
     // will echo 'Our app is running on http://localhost:5000 when run locally'
